eb42f932e5ae0ce8807cb739c69f3c97d7e16788
Ryan Huang <ryan_huang@intuit.com>
2016-08-10 14:55:49 -0700

    updated for heroku

--- ReactWeather/server.js
+++ ReactWeather/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const App = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  }
+  else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 App.use(express.static('public'));
 
-App.listen(3000, () => {
-  console.log('Express server is up on port 3000');
+App.listen(PORT, () => {
+  console.log('Express server is up on port ' + PORT);
 })
